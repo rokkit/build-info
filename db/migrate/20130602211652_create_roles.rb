1ddd55e49c6f7ecbfc7f23bf606a8a2290314e0b
Maksim Ind <fss92@pochta.ru>
2013-06-06 17:38:48 +0400

    Исправлен редирект админа

--- db/migrate/20130602211652_create_roles.rb
+++ db/migrate/20130602211652_create_roles.rb
@@ -1,12 +1,5 @@
 class CreateRoles < ActiveRecord::Migration
-  def migrate(direction)
-    super
-    # Create a default user
-    
-    u=User.create(:email => 'admin@mail.com', :password => 'password', :password_confirmation => 'password') if direction == :up
-    u.roles << Role.new(name:"admin")
-    u.save!
-  end
+
   def change
     create_table :roles do |t|
       t.string :name
--- db/migrate/20130603064647_create_category_articles.rb
+++ db/migrate/20130603064647_create_category_articles.rb
@@ -1,4 +1,12 @@
 class CreateCategoryArticles < ActiveRecord::Migration
+  def migrate(direction)
+    super
+    # Create a default user
+    
+    u=User.create(:email => 'admin@mail.com', :password => 'password', :password_confirmation => 'password') if direction == :up
+    u.roles << Role.new(name:"admin")
+    u.save!
+  end
   def change
     create_table :category_articles do |t|
       t.string :name
