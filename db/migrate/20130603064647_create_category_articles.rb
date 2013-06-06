d2f779527ff04475cb2f2ce482d0daa8a0c3a930
Maksim Ind <fss92@pochta.ru>
2013-06-06 17:52:05 +0400

    Исправлен редирект админа

--- db/migrate/20130603064647_create_category_articles.rb
+++ db/migrate/20130603064647_create_category_articles.rb
@@ -2,11 +2,12 @@ class CreateCategoryArticles < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    
-    u=User.create(:email => 'admin@mail.com', :password => 'password', :password_confirmation => 'password') if direction == :up
-    u.approve!
-    u.roles << Role.new(name:"admin")
-    u.save!
+     if direction == :up
+        u=User.create(:email => 'admin@mail.com', :password => 'password', :password_confirmation => 'password')
+        u.approve!
+        u.roles << Role.new(name:"admin")
+        u.save!
+    end
   end
   def change
     create_table :category_articles do |t|
