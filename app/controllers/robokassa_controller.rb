8ce1837b166d3953b55b2567ce320d76b6a3413a
Maksim Ind <fss92@pochta.ru>
2013-09-04 14:55:19 +0400

    trfx

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -1,16 +1,17 @@
 class RobokassaController < ApplicationController
   include ActiveMerchant::Billing::Integrations
 
-  skip_before_filter :verify_authenticity_token # skip before filter if you chosen POST request for callbacks
+  #skip_before_filter :verify_authenticity_token # skip before filter if you chosen POST request for callbacks
 
-  before_filter :create_notification
+  #before_filter :create_notification
   before_filter :find_payment
 
   # Robokassa call this action after transaction
   def paid
-    if @notification.acknowledge # check if it’s genuine Robokassa request
+    notification = Robokassa.notification request.raw_post, :secret => APP['robokassa']["secret"]
+    if notification.acknowledge # check if it’s genuine Robokassa request
       #@payment.approve! # project-specific code
-      render :text => @notification.success_response
+      render :text => notification.success_response
     else
       head :bad_request
     end
@@ -18,7 +19,8 @@ class RobokassaController < ApplicationController
 
   # Robokassa redirect user to this action if it’s all ok
   def success
-    if !@payment.approved? && @notification.acknowledge
+    notification = Robokassa.notification request.raw_post, :secret => APP['robokassa']["secret_2"]
+    if !@payment.approved? && notification.acknowledge
       @payment.approve!
       redirect_to @payment, :notice => "Robokassa success"
     end
@@ -32,7 +34,7 @@ class RobokassaController < ApplicationController
   private
 
   def create_notification
-    @notification = Robokassa::Notification.new(request.raw_post, :secret => APP['robokassa']['secret_2'])
+    @notification = Robokassa::Notification.new(request.raw_post, :secret => APP['robokassa']['secret'])
   end
 
   def find_payment
