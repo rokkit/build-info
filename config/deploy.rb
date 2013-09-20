92e2435b8540e91bad309f0efcc5aa2f23ace0fd
Maxim Indykov <indmaksim@gmail.com>
2013-09-20 12:08:21 +0400

    add tinymce editor

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,25 +31,25 @@ namespace :deploy do
   end
 end
 set :bundle_flags, "--quiet --binstubs --shebang ruby-local-exec"
-# set :default_environment, {
-#   'RBENV_ROOT' => "$HOME/.rbenv/",
-#   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-# }
+set :default_environment, {
+  'RBENV_ROOT' => "$HOME/.rbenv/",
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 after :deploy do
   run "#{try_sudo} chmod 777 -R #{current_path}/log"
   run "#{try_sudo} chmod 777 -R #{current_path}/tmp"
   run "#{try_sudo} chmod 777 -R #{current_path}/public"
 end
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
+# namespace :deploy do
+#   namespace :assets do
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#     end
+#   end
+# end
